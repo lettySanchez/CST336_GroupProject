bd42c026ebc8c0b768167c52c022c5400db0c590
gopa5979 <agopar@csumb.edu>
2016-04-08 01:04:55 +0000

    connected to database

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
 <?php
+/*
+include('/team_project/CST336_GroupProject/includes/database.php');
+$dbConnection = getDatabaseConnection('catalogue');
+
+function getProductTypes()
+{
+     global $dbConnection;
+    
+    $sql = "SELECT *
+            FROM productType";
+    $statement = $dbConnection->prepare($sql);
+    $statement->execute();
+    $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    //print_r($records);
+    
+    return $records;
+}
+
+function getItems()
+{
+    //array
+    global $dbConnection;
+    
+    $sql = "SELECT *
+            FROM products";
+    $statement = $dbConnection->prepare($sql);
+    $statement->execute();
+    $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    //print_r($records);
+    
+    return $records;
+
+}
+*/
 
 ?>
 <!DOCTYPE html>
@@ -23,8 +59,14 @@
                 ?>
             </select>
             <input type="text" name="searchItem" placeholder="Search for an item" size=100/>
-            <input type="submit" value="submit" name="search">
+            <a target="productsFrame" href="getProducts.php"><input type="submit" value="submit" name="search"/></a>
+            
         </form>
-
+        
+        
+        <div>
+            <iframe name= "productsFrame" width="900" height="900" src="getProducts.php" frameborder="1"></iframe>
+        </div>
+     
     </body>
 </html>
